479b0c21c4881d326bce55b1ba7d8ee43fd0f2aa
Kasumi Okuda <ec2-user@ip-172-31-28-235.ap-northeast-1.compute.internal>
2023-10-02 12:55:04 +0000

    [Add]itemのテーブル作成

--- db/migrate/20231002121422_create_items.rb
+++ db/migrate/20231002121422_create_items.rb
@@ -1,8 +1,22 @@
 class CreateItems < ActiveRecord::Migration[6.1]
   def change
     create_table :items do |t|
-
-      t.timestamps
+      # 商品名
+      t.string :product_name, null: false
+      # 年式
+      t.integer :year, null: false
+      # 走行
+      t.float :mileage, null: false
+      # 車検
+      t.integer :inspection, null: false
+      # 修理
+      t.integer :repair, null: false
+      # 整備
+      t.integer :maintenance, null: false
+      # 税抜価格
+      t.integer :price, null: false
+      # 登録・更新日時
+      t.timestamps null: false
     end
   end
 end
