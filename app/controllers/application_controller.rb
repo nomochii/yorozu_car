e6e35510c1a230bb35dcf6e49f224d02cb4e0c3b
Kasumi Okuda <ec2-user@ip-172-31-28-235.ap-northeast-1.compute.internal>
2023-10-07 07:34:05 +0000

    [Add]顧客用新規登録する際指定したデータは保存できるようコントローラにて許可を追記

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  # ユーザ登録、ログイン認証などが使われる前に
+  # configure_permitted_parametersメソッドが実行されます。
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+protected
+  # ユーザー登録(sign_up)の際に、keys:内のデータ操作を許可しています。
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :first_name_kana, :last_name_kana, :email, :postal_code, :address, :telephone_number, :password])
+  end
 end
