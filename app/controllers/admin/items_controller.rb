4e9b941f4afc118bc83d5a129787452288829021
Kasumi Okuda <ec2-user@ip-172-31-28-235.ap-northeast-1.compute.internal>
2023-10-08 16:40:35 +0000

    [Add]管理者itemコントローラに必要なアクションを定義。投稿画像がない場合の画像を用意しました。

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,2 +1,45 @@
 class Admin::ItemsController < ApplicationController
+  def new
+    
+  end
+
+  def create
+    
+  end
+
+  def index
+    
+  end
+
+
+  def edit
+    
+  end
+
+  def show
+    
+  end
+
+  def destroy
+    
+  end
+
+  def update
+    
+  end
+
+  def get_image
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+  end
+
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :product_name, :introduction, :price)
+  end
 end
+
