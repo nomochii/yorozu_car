23f2c65e08507e2efdacd34a67b359ed1a9a4698
Kasumi Okuda <ec2-user@ip-172-31-28-235.ap-northeast-1.compute.internal>
2023-10-08 13:41:36 +0000

    [Add]顧客の退会処理を定義。退会ボタン押下は管理者側の顧客情報編集作成後に確認します。

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -24,7 +24,11 @@ class Public::CustomersController < ApplicationController
 
 # 顧客退会処理
   def withdraw
-
+    @customer = Customer.find(current_customer.id)
+    @customer.update(is_deleted: true)
+    reset_session
+    flash[:notice] = "退会処理を実行いたしました"
+    redirect_to root_path
   end
 
   def customer_params
