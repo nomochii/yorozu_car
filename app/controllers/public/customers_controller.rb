8e3dc54085615f35687f229ee6612e10cad040f3
Kasumi Okuda <ec2-user@ip-172-31-28-235.ap-northeast-1.compute.internal>
2023-10-08 07:54:52 +0000

    [Add]顧客側ログイン後のヘッダーからマイページに遷移できるようになった

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,21 +1,26 @@
 class Public::CustomersController < ApplicationController
 
+# 顧客マイページ
   def show
-
+    @customer = current_customer
   end
 
+# 顧客マイページ編集
   def edit
 
   end
 
+# 顧客マイページ変更更新
   def update
 
   end
 
+# 顧客退会確認画面
   def unsubscribe
 
   end
 
+# 顧客退会処理
   def withdraw
 
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,25 +2,26 @@ Rails.application.routes.draw do
 
   # トップ画面
 
-  root to: "public/homes#top"
+  scope module: :public do
+  root to: "homes#top"
   # 会社概要
-  get "company_profile" => "public/homes#company_profile", as: "company_profile"
+  get "company_profile" => "homes#company_profile", as: "company_profile"
   get "customers/my_page" => "customers#show"
   resources :customers, only: [:edit, :update, :index]
   # 顧客用
-  # URL /customers/sign_in ...
+ end
+
+ # 顧客ログイン・新規登録
   devise_for :customers,skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
-
   # 顧客用ゲストログイン
   devise_scope :customer do
   post 'customer/guest_sign_in', to: 'public/sessions#guest_sign_in'
   end
-
   # 管理者用
-  # URL /admin/sign_in ...
+  # 管理者ログイン
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
     sessions: "admin/sessions"
   }
