b934d8a9ee3b83a5760b562795b1510d850258f7
Kasumi Okuda <ec2-user@ip-172-31-28-235.ap-northeast-1.compute.internal>
2023-10-08 17:49:19 +0000

    [Add]管理者のログイン後管理者用のトップページに遷移するようにしました。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,25 @@ class ApplicationController < ActionController::Base
   # configure_permitted_parametersメソッドが実行されます。
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+private
+def after_sign_in_path_for(resource_or_scope)
+  if resource_or_scope.is_a?(Admin)
+        admin_path
+  else
+        root_path
+  end
+end
+
+def after_sign_out_path_for(resource_or_scope)
+  if resource_or_scope == :customer
+        root_path
+  elsif resource_or_scope == :admin
+        new_admin_session_path
+  else
+        root_path
+  end
+end
+
 protected
   # ユーザー登録(sign_up)の際に、keys:内のデータ操作を許可しています。
   def configure_permitted_parameters
