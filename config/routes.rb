3390b9f233ff03f7dbe4db7865086e34fbe2fa01
Kasumi Okuda <ec2-user@ip-172-31-28-235.ap-northeast-1.compute.internal>
2023-10-08 07:33:37 +0000

    [Add]顧客側customerのルーティング作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,22 @@
 Rails.application.routes.draw do
 
   # トップ画面
-  scope module: :public do
+
   root to: "public/homes#top"
   # 会社概要
   get "company_profile" => "public/homes#company_profile", as: "company_profile"
-  resources :customers, only: [:show, :edit, :update, :index]
-
+  get "customers/my_page" => "customers#show"
+  resources :customers, only: [:edit, :update, :index]
   # 顧客用
   # URL /customers/sign_in ...
   devise_for :customers,skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
-  end
+
   # 顧客用ゲストログイン
-   devise_scope :customer do
-    post 'customer/guest_sign_in', to: 'public/sessions#guest_sign_in'
+  devise_scope :customer do
+  post 'customer/guest_sign_in', to: 'public/sessions#guest_sign_in'
   end
 
   # 管理者用
