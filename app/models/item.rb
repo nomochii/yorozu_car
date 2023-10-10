5c283fd766624cea51a505b816c03b01b3e88b4d
Kasumi Okuda <ec2-user@ip-172-31-28-235.ap-northeast-1.compute.internal>
2023-10-10 03:12:48 +0000

    [Add]計算式はモデルにも記述できるという記事を見つけたので、試しにitemモデルにitemの税込価格を定義してみました。

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,9 @@ class Item < ApplicationRecord
    has_one_attached :image
    has_many :order_details, dependent: :destroy
    has_many :cart_items, dependent: :destroy
+
+   # 税込価格(税率10%)
+   def  add_tax_price
+      (self.price * 1.10).floor
+   end
 end
