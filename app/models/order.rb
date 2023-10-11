7d6941967f6116071d6980f3947fee46a8d58f51
Kasumi Okuda <ec2-user@ip-172-31-28-235.ap-northeast-1.compute.internal>
2023-10-11 02:25:35 +0000

    [Add]enum（支払い方法）を定義する

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,8 @@
 class Order < ApplicationRecord
   belongs_to :customer
   has_many :order_details, dependent: :destroy
+
+# payment_method(支払い方法)カラムに入る内容
+  enum payment_method: { credit_card: 0, transfer: 1 }
+
 end
