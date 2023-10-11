d79504615734f095f5ae1056197bb0d7faa027ff
Kasumi Okuda <ec2-user@ip-172-31-28-235.ap-northeast-1.compute.internal>
2023-10-11 02:31:45 +0000

    [Add]Railsアプリケーションのデフォルトの言語が日本語となる

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module YorozuCar
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+    # Rails アプリケーションのデフォルトの言語が日本語となる
+    config.i18n.default_locale = :ja
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
