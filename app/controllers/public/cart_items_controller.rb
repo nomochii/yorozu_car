5a7b4ba527f445a9edef2d7c60796f30a9f548dc
Kasumi Okuda <ec2-user@ip-172-31-28-235.ap-northeast-1.compute.internal>
2023-10-14 15:17:21 +0000

    [Add]顧客側cart_itemsコントローラのindexアクションに合計金額が表示できるよう定義しました。

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -2,6 +2,8 @@ class Public::CartItemsController < ApplicationController
 
   def index
     @cart_items = current_customer.cart_items
+    # 合計金額
+    @total = @cart_items.inject(0) { |sum, item| sum + item.subtotal }
   end
 
   def update
