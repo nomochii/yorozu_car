a88e56bbcb26826c1e794f60b7907ed655daa65e
Kasumi Okuda <ec2-user@ip-172-31-28-235.ap-northeast-1.compute.internal>
2023-10-15 16:41:31 +0000

    [Add]顧客側ordersコントローラに必要なアクションを定義しました。

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,2 +1,21 @@
 class Public::OrdersController < ApplicationController
+
+  def new
+  end
+
+  def create
+  end
+
+  def index
+  end
+
+  def confirm
+  end
+
+  def complete
+  end
+
+  def show
+  end
+
 end
