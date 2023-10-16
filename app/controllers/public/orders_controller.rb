b7a214f379fde15048e022bef64239e4da3331f5
Kasumi Okuda <ec2-user@ip-172-31-28-235.ap-northeast-1.compute.internal>
2023-10-16 11:12:26 +0000

    [Add]顧客側ordersの注文情報確認画面作成

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -14,7 +14,21 @@ class Public::OrdersController < ApplicationController
   def index
   end
 
+  # new 画面から渡ってきたデータをユーザーに確認してもらう
   def confirm
+     # new 画面から渡ってきたデータを @order に入れる
+    @order = Order.new(order_params)
+    @order.shipping_postal_code = current_customer.postal_code
+    @order.shipping_address = current_customer.address
+    # フルネーム
+    @order.shipping_name = current_customer.first_name + current_customer.last_name
+    # 送料
+    @order.postage = 800
+    @cart_items = current_customer.cart_items
+    # カート内の合計金額
+    @total = @cart_items.inject(0) { |sum, item| sum + item.subtotal }
+    # 請求金額
+    @order.total_amount = @total + @order.postage
   end
 
   def complete
@@ -22,5 +36,10 @@ class Public::OrdersController < ApplicationController
 
   def show
   end
+
+  private
+    def order_params
+      params.require(:order).permit(:payment_method)
+    end
 
 end
