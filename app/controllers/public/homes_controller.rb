1100243ff5d17afa49e11df6de868b603356f988
Kasumi Okuda <ec2-user@ip-172-31-28-235.ap-northeast-1.compute.internal>
2023-10-19 12:16:24 +0000

    [Add]顧客側のトップページに

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,9 +1,11 @@
 class Public::HomesController < ApplicationController
+  # トップページ
   def top
-
+    @items = Item.all
+    # 商品を新着順、４件表示します。
+    @items = Item.order('id DESC').limit(4)
   end
-
+  # 会社概要ページ
   def company_profile
-
   end
 end
